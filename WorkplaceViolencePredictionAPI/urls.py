728907db1858f3e48275bdbd4635c2720074a0d3
Avery Bobbitt <avery@bobbitt.dev>
2024-02-12 14:47:29 -0500

    set up url router

--- WorkplaceViolencePredictionAPI/urls.py
+++ WorkplaceViolencePredictionAPI/urls.py
@@ -16,8 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+
+from WorkplaceViolencePredictionAPI.api import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    path('api/', include("api.urls")),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += router.urls
