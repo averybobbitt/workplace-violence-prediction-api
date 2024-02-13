b694bfc4e0b80c5e58f58c5087cbefde2a2ce775
Avery Bobbitt <bobbit82@rowan.edu>
2024-02-13 16:58:40 -0500

    added short overview for the file and its purpose

--- WorkplaceViolencePredictionAPI/API/serializers.py
+++ WorkplaceViolencePredictionAPI/API/serializers.py
@@ -1,6 +1,15 @@
 from django.contrib.auth.models import Group, User
 from rest_framework import serializers
 
+"""
+Serializers allow complex data such as querysets and model instances to be converted to native Python datatypes that can
+then be easily rendered into JSON, XML or other content types. Serializers also provide deserialization, allowing parsed
+data to be converted back into complex types, after first validating the incoming data. The serializers in REST
+framework work very similarly to Django's Form and ModelForm classes. We provide a Serializer class which gives you a 
+powerful, generic way to control the output of your responses, as well as a ModelSerializer class which provides a 
+useful shortcut for creating serializers that deal with model instances and querysets.
+"""
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
