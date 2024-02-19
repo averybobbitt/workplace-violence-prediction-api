bdce81be91dd40cdfa50dc04e9cd7a88d7243aac
profic93 <profic93@students.rowan.edu>
2024-02-19 13:27:59 -0500

    Added function to generate random percentage of inpatient beds

--- data_gen.py
+++ data_gen.py
@@ -1,4 +1,6 @@
 import random
+import numpy as np
+from numpy.random import Generator
 
 # Generates a single instance of gender data based on the gender distribution
 # of staff within hospitals. Gives a 25/75, M/F split.
@@ -11,6 +13,13 @@ def generate_gender_data():
     else:
         gender = 'F'
     return gender
+
+# Generates random percentage of beds occupied.
+# Default mean and standard deviation comes from CDC dataset
+# Returns ndarray or scalar based on number of samples
+def generate_inpatient_bed_occupancy_data(mean=0.721760392289664, stdDev=0.08738743450179379, samples=1):
+    randomNum = Generator.normal(mean, stdDev, samples)
+    return randomNum
 
 if __name__ == '__main__':
     print("Test data generation")
