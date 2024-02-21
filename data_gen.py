d01fe00a2eaab32b5f95a95340d9e6c671ce5645
profic93 <profic93@students.rowan.edu>
2024-02-21 11:35:49 -0500

    Added data gen for nurse, patients, and time of day

--- data_gen.py
+++ data_gen.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 from numpy.random import Generator
+import datetime
 
 # Generates a single instance of gender data based on the gender distribution
 # of staff within hospitals. Gives a 25/75, M/F split.
@@ -18,8 +19,23 @@ def generate_gender_data():
 # Default mean and standard deviation comes from CDC dataset
 # Returns ndarray or scalar based on number of samples
 def generate_inpatient_bed_occupancy_data(mean=0.721760392289664, stdDev=0.08738743450179379, samples=1):
-    randomNum = Generator.normal(mean, stdDev, samples)
-    return randomNum
+    inpatient_bed_occupancy = Generator.normal(mean, stdDev, samples)
+    return inpatient_bed_occupancy
+
+def generate_number_of_nurses(mean=5.153, stdDev=.5652, samples=1):
+    nurses = Generator.normal(mean, stdDev, samples)
+    return nurses
+
+def generate_number_of_patients(mean=66.9516, stdDev=6.953, samples=1):
+    patients = Generator.normal(mean, stdDev, samples)
+    return patients
+
+def generate_time_of_day():
+    hour = random.randrange(24)
+    minute = random.randrange(60)
+    time_of_day = datetime.time(hour, minute)
+    return time_of_day
+
 
 if __name__ == '__main__':
     print("Test data generation")
