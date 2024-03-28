0d0832e6201ec23546df039f7f94e1f3f4ad79a5
Avery Bobbitt <bobbit82@rowan.edu>
2024-03-28 10:17:00 -0400

    nuked celery config, this bg task thing fuckin sucks

--- WorkplaceViolencePredictionAPI/API/tasks.py
+++
@@ -1,8 +0,0 @@
-from celery import shared_task
-
-
-@shared_task
-def my_task(arg1, arg2):
-    # Task logic here
-    result = arg1 + arg2
-    return result
--- WorkplaceViolencePredictionAPI/settings.py
+++ WorkplaceViolencePredictionAPI/settings.py
@@ -125,6 +125,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-# Celery settings
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
