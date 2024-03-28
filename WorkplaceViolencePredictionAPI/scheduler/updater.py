1c1d340c84b2947ee1e3cadd92e2f07a6d6d0a02
Avery Bobbitt <bobbit82@rowan.edu>
2024-03-28 11:08:13 -0400

    cleaned up(?) updater

--- WorkplaceViolencePredictionAPI/scheduler/updater.py
+++ WorkplaceViolencePredictionAPI/scheduler/updater.py
@@ -2,8 +2,16 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 from WorkplaceViolencePredictionAPI.scheduler import tasks
 
+# (tasks.FUNCTION, INTERVAL_IN_SECONDS)
+jobs = [
+    (tasks.hello_world, 5)
+]
+
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(tasks.hello_world, 'interval', seconds=10)
+
+    for job in jobs:
+        scheduler.add_job(job[0], 'interval', seconds=job[1])
+
     scheduler.start()
