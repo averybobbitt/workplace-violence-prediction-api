65ed4413806612c524f76644ab2f159e2b9d02d1
Avery Bobbitt <bobbit82@rowan.edu>
2024-03-28 17:48:15 -0400

    modified how tasks are added to scheduler

--- WorkplaceViolencePredictionAPI/scheduler/tasks.py
+++ WorkplaceViolencePredictionAPI/scheduler/tasks.py
@@ -1,2 +1,6 @@
 def hello_world():
     print("hello!")
+
+
+def add(x, y):
+    return x + y
--- WorkplaceViolencePredictionAPI/scheduler/updater.py
+++ WorkplaceViolencePredictionAPI/scheduler/updater.py
@@ -2,16 +2,10 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 from WorkplaceViolencePredictionAPI.scheduler import tasks
 
-# (tasks.FUNCTION, INTERVAL_IN_SECONDS)
-jobs = [
-    (tasks.hello_world, 5)
-]
-
 
 def start():
     scheduler = BackgroundScheduler()
 
-    for job in jobs:
-        scheduler.add_job(job[0], 'interval', seconds=job[1])
+    scheduler.add_job(tasks.hello_world, 'interval', seconds=10)
 
     scheduler.start()
