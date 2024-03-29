ab0d6613d69c9ca368d6888ed1650799e940cb52
Avery Bobbitt <119345006+averybobbitt@users.noreply.github.com>
2024-03-29 11:22:18 -0400

    Update settings.py
    Signed-off-by: Avery Bobbitt <119345006+averybobbitt@users.noreply.github.com>

--- WorkplaceViolencePredictionAPI/settings.py
+++ WorkplaceViolencePredictionAPI/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework.authtoken",
-    "background_task",
     "WorkplaceViolencePredictionAPI.API",
 ]
 
