29de0f23398c9e46b24ac91b645da0d8f073d280
dipiet77 <dipiet77@students.rowan.edu>
2024-04-01 16:45:36 -0400

    changes probabilites in Prediction to be between 0 and 1

--- WorkplaceViolencePredictionAPI/API/views.py
+++ WorkplaceViolencePredictionAPI/API/views.py
@@ -157,7 +157,7 @@ class PredictionModelViewSet(viewsets.ModelViewSet):
         new_entry = {
             "hData": queryset.id,
             "wpvRisk": prediction,
-            "wpvProbability": probabilities*100
+            "wpvProbability": probabilities
         }
         serializer = self.get_serializer(data=new_entry, many=False)
         try:
