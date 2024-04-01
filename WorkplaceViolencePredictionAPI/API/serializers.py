0eba2e88ac3943d22a9eb8bea2562778fadd7206
Avery Bobbitt <bobbit82@rowan.edu>
2024-04-01 16:34:27 -0400

    emergency fix

--- WorkplaceViolencePredictionAPI/API/serializers.py
+++ WorkplaceViolencePredictionAPI/API/serializers.py
@@ -3,7 +3,6 @@ from rest_framework import serializers
 
 from WorkplaceViolencePredictionAPI.API.models import HospitalData, TrainingData, RiskData, IncidentLog
 
-
 """
 Serializers allow complex data such as querysets and model instances to be converted to native Python datatypes that can
 then be easily rendered into JSON, XML or other content types. Serializers also provide deserialization, allowing parsed
@@ -24,22 +23,21 @@ class HospitalDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = HospitalData
         fields = "__all__"
-        
-        
- class TrainingDataSerializer(serializers.ModelSerializer):
+
+
+class TrainingDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = TrainingData
         fields = "__all__"
-        
+
 
 class RiskDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = RiskData
         fields = "__all__"
-        
-        
+
+
 class IncidentDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = IncidentLog
         fields = "__all__"
-        
