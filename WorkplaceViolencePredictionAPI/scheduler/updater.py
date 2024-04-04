32e1891a4fb155aca771b43c99e89c6dc8386cf2
dipiet77 <dipiet77@students.rowan.edu>
2024-04-04 16:18:29 -0400

    Added scheduler that checks for 6 month old indient log entries every hour and deletes them

--- WorkplaceViolencePredictionAPI/scheduler/updater.py
+++ WorkplaceViolencePredictionAPI/scheduler/updater.py
@@ -6,8 +6,8 @@ from WorkplaceViolencePredictionAPI.scheduler import tasks
 def start():
     scheduler = BackgroundScheduler(job_defaults={'max_instances': 10})
 
-    #scheduler.add_job(tasks.get_data, 'interval', seconds=10)
-    #scheduler.add_job(tasks.predict, 'interval', seconds=10)
+    scheduler.add_job(tasks.get_data, 'interval', seconds=10)
+    scheduler.add_job(tasks.predict, 'interval', seconds=10)
     scheduler.add_job(tasks.check_old_logs, 'interval', seconds=3600)
 
     scheduler.start()
