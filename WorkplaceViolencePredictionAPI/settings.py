43eac0e3dbda2d20a27f310715703f0b5d4fd6aa
Avery Bobbitt <bobbit82@rowan.edu>
2024-04-05 00:02:17 -0400

    added logging settings to settings.py

--- WorkplaceViolencePredictionAPI/settings.py
+++ WorkplaceViolencePredictionAPI/settings.py
@@ -81,6 +81,28 @@ TEMPLATES = [
     },
 ]
 
+if not os.path.exists("/logs"):
+    os.mkdir("/logs")
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": "/logs/debug.log",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+    },
+}
+
 WSGI_APPLICATION = "WorkplaceViolencePredictionAPI.wsgi.application"
 ASGI_APPLICATION = "WorkplaceViolencePredictionAPI.asgi.application"
 
