67dd756ad0940e5370f68027951cf84d0186f16b
Avery Bobbitt <bobbit82@rowan.edu>
2024-04-07 19:53:19 -0400

    slightly modified how the dials work to help modularity, feel free to revert if more confusing

--- WorkplaceViolencePredictionAPI/API/static/js/dial.js
+++ WorkplaceViolencePredictionAPI/API/static/js/dial.js
@@ -1,27 +1,36 @@
-const changeNum = () => {
-    const randomNum = Math.round(Math.random() * 100);
+window.onload = function () {
+    const gaugeElement = document.getElementById("demoGauge");
+
+    setInterval(() => {
+        setGaugeValue(gaugeElement, Math.random().toFixed(2));
+    }, 2000);
+};
+
+function setGaugeValue(gauge, value) {
+    if (value < 0 || value > 1) return;
+
+    gauge.querySelector(".gauge_fill").style.transform = `rotate(${value / 2}turn)`;
+    gauge.querySelector(".gauge_percentage").innerText = Math.round(value * 100);
+}
+
+function changeNum() {
+    const randomNum = Math.random().toFixed(2);
     const degrees = Math.round((randomNum / 100) * 180);
     const root = document.querySelector(":root");
     let title = document.querySelector(".loader__title");
-  
+
     let currentNumber = title.innerText;
-    
+
     setInterval(() => {
-       if (currentNumber < randomNum) {
-        currentNumber++;
-        title.innerText = currentNumber;
-      } else if (currentNumber > randomNum) {
-        currentNumber--;
-        title.innerText = currentNumber;
-      }
+        if (currentNumber < randomNum) {
+            currentNumber++;
+            title.innerText = currentNumber;
+        } else if (currentNumber > randomNum) {
+            currentNumber--;
+            title.innerText = currentNumber;
+        }
     }, 3);
-  
+
     root.style.setProperty("--rotation", `${degrees}deg`);
-  };
-  
-  
-  setInterval(() => {
-    changeNum();
-  }, 2000);
-  
-  
+}
+
