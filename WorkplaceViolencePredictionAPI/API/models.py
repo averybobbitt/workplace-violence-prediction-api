4436178e3bd4f4a01deb3430510ce1873ce2d100
Avery Bobbitt <bobbit82@rowan.edu>
2024-04-15 15:53:33 -0400

    [!] fixed model types and dummy api image source

--- WorkplaceViolencePredictionAPI/API/models.py
+++ WorkplaceViolencePredictionAPI/API/models.py
@@ -9,7 +9,7 @@ from django.db import models
 
 
 class HospitalData(models.Model):
-    id = models.SmallAutoField(primary_key=True, editable=False)
+    id = models.BigAutoField(primary_key=True, editable=False)
     createdTime = models.DateTimeField(db_column="createdTime", auto_now_add=True, editable=False)
     avgNurses = models.DecimalField(db_column="avgNurses", max_digits=20, decimal_places=10)
     avgPatients = models.DecimalField(db_column="avgPatients", max_digits=20, decimal_places=10)
@@ -23,7 +23,7 @@ class HospitalData(models.Model):
 
 
 class TrainingData(models.Model):
-    id = models.SmallAutoField(primary_key=True, editable=False)
+    id = models.BigAutoField(primary_key=True, editable=False)
     createdTime = models.DateTimeField(db_column="createdTime", auto_now_add=True, editable=False)
     avgNurses = models.DecimalField(db_column="avgNurses", max_digits=20, decimal_places=10)
     avgPatients = models.DecimalField(db_column="avgPatients", max_digits=20, decimal_places=10)
@@ -38,7 +38,7 @@ class TrainingData(models.Model):
 
 
 class RiskData(models.Model):
-    id = models.SmallAutoField(primary_key=True, editable=False)
+    id = models.BigAutoField(primary_key=True, editable=False)
     hData = models.ForeignKey(HospitalData, null=True, on_delete=models.CASCADE)
     wpvRisk = models.BooleanField(db_column="wpvRisk")
     wpvProbability = models.DecimalField(db_column="wpvProbability", max_digits=3, decimal_places=2)
@@ -50,7 +50,7 @@ class RiskData(models.Model):
 
 
 class IncidentLog(models.Model):
-    id = models.SmallAutoField(primary_key=True, editable=False)
+    id = models.BigAutoField(primary_key=True, editable=False)
     incidentType = models.CharField(db_column="incidentType", max_length=255)
     incidentDate = models.DateTimeField(db_column="incidentDate")
     affectedPeople = models.CharField(db_column="affectedPeople", max_length=255)
