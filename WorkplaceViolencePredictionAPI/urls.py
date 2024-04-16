81900ff2c8ad11a404f508c3e9a521ca7a15c2bd
Avery Bobbitt <bobbit82@rowan.edu>
2024-04-15 22:05:22 -0400

    reworked urls

--- WorkplaceViolencePredictionAPI/urls.py
+++ WorkplaceViolencePredictionAPI/urls.py
@@ -29,6 +29,17 @@ router.register(r"train", views.TrainingDataViewSet)
 router.register(r"model", views.PredictionModelViewSet)
 router.register(r"log", views.IncidentLogViewSet)
 
+hello_urls = [
+    path("world/", views.HelloView.as_view()),
+    path("admin/", views.HelloView.as_view()),
+]
+
+email_urls = [
+    path("send/", views.EmailView.as_view()),
+    path("append/", views.EmailView.as_view()),
+    path("remove/", views.EmailView.as_view()),
+]
+
 urlpatterns = [
     path("admin/", admin.site.urls),  # built-in admin portal for Django
     # Webpage Routes
@@ -39,6 +50,6 @@ urlpatterns = [
     path("api/", include(router.urls)),  # router paths defined above
     path("api/auth/", include("rest_framework.urls")),  # login/out for browser view
     path("api/token/", obtain_auth_token),
-    path("api/hello/", views.HelloView.as_view()),
-    path("api/email/", views.EmailView.as_view()),
+    path("api/hello/", include(hello_urls)),
+    path("api/email/", include(email_urls)),
 ]
