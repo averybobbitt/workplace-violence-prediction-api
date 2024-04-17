cbf25901ba4f0af569facbc0707a8e2c964b025b
Anthony-Ung-931 <makeanthonygreatagain@gmail.com>
2024-04-17 15:07:12 -0400

    In log.js, got the elements from the HTML form

--- WorkplaceViolencePredictionAPI/API/static/js/log.js
+++ WorkplaceViolencePredictionAPI/API/static/js/log.js
@@ -1,3 +1,6 @@
 function submitIncident() {
-
+    let incidentType = document.getElementById("incidentType").value;
+    let incidentDate = document.getElementById("incidentDate").value;
+    let affectedPeople = document.getElementById("affectedPeople").value;
+    let incidentDescription = document.getElementById("incidentDescription").value;
 }
