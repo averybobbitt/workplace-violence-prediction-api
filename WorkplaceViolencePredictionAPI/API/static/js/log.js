3e800bf5b18fac2f7a3ad94c3aca1cea689872f3
Anthony-Ung-931 <makeanthonygreatagain@gmail.com>
2024-04-17 15:11:06 -0400

    Basically refactored addEmail() from email.js

--- WorkplaceViolencePredictionAPI/API/static/js/log.js
+++ WorkplaceViolencePredictionAPI/API/static/js/log.js
@@ -3,4 +3,24 @@ function submitIncident() {
     let incidentDate = document.getElementById("incidentDate").value;
     let affectedPeople = document.getElementById("affectedPeople").value;
     let incidentDescription = document.getElementById("incidentDescription").value;
+
+    console.log(JSON.stringify({
+        "incidentType": incidentType
+        "incidentDate": incidentDate
+        "affectedPeople": affectedPeople
+        "incidentDescription": incidentDescription
+    }));
+
+    fetch("http://localhost:8000/api/email/", {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "incidentType": incidentType
+
+        })"incidentDate": incidentDate
+            "affectedPeople": affectedPeople
+            "incidentDescription": incidentDescription
+    }).then(r => console.log(`Incident Reported.`));
 }
